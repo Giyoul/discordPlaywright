d7ecfe7c3356ca34d77719f00c83d9e4653acd8e
Giyoul <cla12093@gmail.com>
2025-11-16 16:41:38 +0900

    feat: add message print feature

--- controller/app_controller.py
+++ controller/app_controller.py
@@ -1,3 +1,8 @@
-
+from view.cli_view import CLIView
 
 class AppController:
+    def __init__(self):
+        self.view = CLIView()
+
+    def run(self):
+        self.view.show_welcome()
---
+++ view/cli_view.py
@@ -0,0 +1,18 @@
+
+class CLIView:
+    def show_welcome(self):
+        print("\n<디스코드 데이터 수집>")
+
+    def prompt_main_menu(self):
+        print("\n메뉴 선택:")
+        print("1. 디스코드 글/ 댓글 스크래핑")
+        print("2. 카테고리 분류")
+        print("3. CSV 저장")
+        print("0. 종료")
+        return input("> ")
+
+    def show_message(self, msg):
+        print(f"[+] {msg}")
+
+    def show_error(self, err):
+        print(f"[ERROR] {err}")
